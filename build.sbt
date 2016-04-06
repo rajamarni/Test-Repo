c168352fc513ac717dd65c8afc00badff0c4b866
tarak <raja.marni@gmail.com>
2016-04-06 17:49:35 +0530

    Discription about each job -------------------------- Hadoop-Spark-GraphX: Load data(input triple filles) into HDFS(by using HDFS commands)
    Job1:
    Read data files(ntriples) from HDFS, parse triples, Identify patients and create GraphX input format , patient files back in HDFS.
    
    Job2:
    Load GraphX input format, patient files(output files from JOB1).Use this input create graph and apply pregel algorithm, and output of the pregel algorithm(quads) is writern to HDFS.
    
    Job3:
    Load patients file and output file of job2 to process and generate nquads file by patient wise and write back to HDFS.
    
    Hadoop-Spark-Cassandra:
    Load data(input triple filles) into HDFS(by using HDFS commands)
    Manually create cassandra keyspace(DB) and Table which is hash partitioned by subject-prediicate-object
    
    Job1:
    Load using spark job data from HDFS into cassandra (keyspace) table
    
    Job2:
    spark job to recursively iterates over the patient inverted tree of triples and update each triple with the patient-id and write into a file name with patient-id into linux filesystem

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "SDLJOB1 Project"
+version := "1.1"
+scalaVersion := "2.10.4"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.0"
---
+++ src/main/scala/test.scala
@@ -0,0 +1,14 @@
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.SparkConf
+
+object test{
+	def main(args: array[String]){
+		var conf = new SparkConf()
+		conf.setAppName("Test app")
+		var sc = new SparkContext(conf)
+		
+		val z = sc.parallelize(100 to 120, 5)
+		val r = z.zipWithUniqueId
+		println(r.collect)
+	}
+}
